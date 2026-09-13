312886fdc884c081f10e7d963d81a4997c00608e
Keyur <ST10472464@vcconnect.edu.za>
2026-09-13 16:36:19 +0200

    Add MenuItem entity model with PartitionKey and RowKey support

---
+++ Cloud Development B CLDV 6212- POE Part 1/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace Cloud_Development_B_CLDV_6212__POE_Part_1
+{
+    // (Microsoft Learn, 2026) (ITableEntity Interface)
+    public class MenuItem : ITableEntity
+    {
+        public string PartitionKey { get; set; } = default!;
+
+        public string RowKey { get; set; } = default!;
+
+        public ETag ETag { get; set; } = default!;
+        public DateTimeOffset? Timestamp { get; set; } = default!;
+
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public double Price { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
